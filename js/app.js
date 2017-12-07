6068ea814505c065c29ca973407b964e3b80b83a
arniroman <arniroman949@gmail.com>
2017-12-07 21:46:58 +0200

    fixed ui, removed unnecessary code, cleanup

--- js/app.js
+++ js/app.js
@@ -6,9 +6,6 @@ app.config(['$routeProvider', function($routeProvide) {
         .when('/', {
             templateUrl: 'template/home.html'
         })
-        .when('/home', {
-            templateUrl: 'template/home.html'
-        })
         .when('/create', {
             templateUrl: 'template/create.html'
         })
@@ -21,71 +18,63 @@ app.config(['$routeProvider', function($routeProvide) {
 }]);
 
 /*-- list home controller --*/
-app.controller('postsHomeCtrl', function($scope, _save, _read) {
-    $scope.list = _read()
+app.controller('postsHomeCtrl', function($scope, save, read) {
+    $scope.list = read()
     $scope.removedPost = function(list) {
         $scope.list.forEach(function(elem, index) {
             if (elem.id == list.id) {
                 $scope.list.splice(index, 1)
-                _save($scope.list)
+                save($scope.list)
             }
         });
     }
 })
 
 /*-- create posts controller --*/
-app.controller('createPostCtrl', function($scope, _save, _read) {
-    $scope.list = _read()
+app.controller('createPostCtrl', function($scope, save, read) {
+    $scope.list = read()
     $scope.ID = function() {
         return '_' + Math.random().toString(36).substr(2, 9);
     };
 
     $scope.newPostBtn = function() {
-        if ($scope.newlistPost != undefined) {
-            $scope.list.push({
-                id: $scope.ID(),
-                name: $scope.newlistPost,
-                removed: true,
-                coment: []
-            })
-        }
-        _save($scope.list)
+        if ($scope.newlistPost === undefined) return
+        $scope.list.push({
+            id: $scope.ID(),
+            name: $scope.newlistPost,
+            coment: []
+        })
+        save($scope.list)
     }
     $scope.data = $scope.list
 })
 
-/*-- create coments controller --*/
-app.controller('createCommentsCtrl', function($scope, $routeParams, _save, _read) {
-    $scope.list = _read()
+/*-- create comments controller --*/
+app.controller('createCommentsCtrl', function($scope, $routeParams, save, read) {
+    $scope.list = read()
     let postId = $routeParams.postId
     $scope.post = _.find($scope.list, { id: postId })
     $scope.addNewComent = function() {
         $scope.list.filter(function(elem) {
             if (elem.id == postId && $scope.newComment != undefined) {
                 elem.coment.push($scope.newComment)
-                _save($scope.list)
+                save($scope.list)
             }
         })
     }
 })
 
 /*-- functions fabric for save in local storage --*/
-app.factory('_save', function() {
-    return function _save(data) {
-        window.localStorage["tasks"] = JSON.stringify(data, function(key, val) {
-            if (key == '$$hashKey') {
-                return undefined;
-            }
-            return val
-        });
+app.factory('save', () => {
+    return (data) => {
+        window.localStorage["tasks"] = JSON.stringify(data);
     }
 })
 
-app.factory('_read', function() {
-    return function _read(data) {
-        var temp = window.localStorage["tasks"]
-        if (!temp) data = [];
-        else data = JSON.parse(temp);
-        return data;
+app.factory('read', () => {
+    return () => {
+        let temp = window.localStorage["tasks"]
+        if (!temp) return [];
+        return JSON.parse(temp);
     }
 })
